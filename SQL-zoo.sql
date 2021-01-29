b78182180bdceb7bb9044e8bf732e37801b2734a
jpdf00 <j.pdf00@gmail.com>
2021-01-29 13:07:29 -0300

    Create SQL-zoo.sql

---
+++ SQL-zoo.sql
@@ -0,0 +1,12 @@
+-- Tutorial 0: SELECT Basics
+  -- Exercise 0.1:
+  SELECT population FROM world
+  WHERE name = 'Germany'
+
+  -- Exercise 0.2:
+  SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+  -- Exercise 0.3:
+  SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
