c750d60cafe88083f702027fa37ae188d6534b4f
jpdf00 <j.pdf00@gmail.com>
2021-02-01 17:25:44 -0300

    Update SQL-zoo.sql
    Add Tutorial 8

--- SQL-zoo.sql
+++ SQL-zoo.sql
@@ -502,3 +502,123 @@
   AND name != 'Art Garfunkel'
 
 --
+
+-- Tutorial 8: Using Null
+  -- Exercise 8.1: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name FROM teacher
+  LEFT JOIN dept
+    ON teacher.dept = dept.id
+  WHERE dept.name IS NULL
+
+  -- Exercise 8.2: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, dept.name FROM teacher
+  INNER JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.3: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, dept.name
+  FROM teacher
+  LEFT JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.4: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, dept.name
+  FROM teacher
+  RIGHT JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.5: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT name, COALESCE(mobile, '07986 444 2266') AS mobile FROM teacher
+
+  -- Exercise 8.6: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, COALESCE(dept.name, 'None')
+  FROM teacher
+  LEFT JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.7: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT COUNT(name), COUNT(mobile) FROM teacher
+
+  -- Exercise 8.8: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT dept.name, COUNT(teacher.name)
+  FROM teacher
+  RIGHT JOIN dept
+    ON teacher.dept = dept.id
+  GROUP BY dept.name
+
+  -- Exercise 8.9: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT name, CASE WHEN dept = 1 THEN 'Sci'
+                    WHEN dept = 2 THEN 'Sci'
+                    ELSE 'Art'
+                    END
+  FROM teacher
+
+  -- Exercise 8.10: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT name, CASE WHEN dept = 1 THEN 'Sci'
+                    WHEN dept = 2 THEN 'Sci'
+                    WHEN dept = 3 THEN 'Art'
+                    ELSE 'None'
+                    END
+  FROM teacher
+
+--
+
+-- Tutorial 8: Using Null
+  -- Exercise 8.1: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name FROM teacher
+  LEFT JOIN dept
+    ON teacher.dept = dept.id
+  WHERE dept.name IS NULL
+
+  -- Exercise 8.2: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, dept.name FROM teacher
+  INNER JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.3: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, dept.name
+  FROM teacher
+  LEFT JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.4: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, dept.name
+  FROM teacher
+  RIGHT JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.5: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT name, COALESCE(mobile, '07986 444 2266') AS mobile FROM teacher
+
+  -- Exercise 8.6: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT teacher.name, COALESCE(dept.name, 'None')
+  FROM teacher
+  LEFT JOIN dept
+    ON teacher.dept = dept.id
+
+  -- Exercise 8.7: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT COUNT(name), COUNT(mobile) FROM teacher
+
+  -- Exercise 8.8: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT dept.name, COUNT(teacher.name)
+  FROM teacher
+  RIGHT JOIN dept
+    ON teacher.dept = dept.id
+  GROUP BY dept.name
+
+  -- Exercise 8.9: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT name, CASE WHEN dept = 1 THEN 'Sci'
+                    WHEN dept = 2 THEN 'Sci'
+                    ELSE 'Art'
+                    END
+  FROM teacher
+
+  -- Exercise 8.10: NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+  SELECT name, CASE WHEN dept = 1 THEN 'Sci'
+                    WHEN dept = 2 THEN 'Sci'
+                    WHEN dept = 3 THEN 'Art'
+                    ELSE 'None'
+                    END
+  FROM teacher
+
+--
